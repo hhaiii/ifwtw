dcf52ab613f12b9b38127f13f0d729d3b32cdabd
stefano@interact.it <Jeko@JekBook-Air.local>
2013-05-16 19:12:17 +0200

    prova per heroku

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var sys = require('sys');
 var xmpp = require('node-xmpp');
-//var http = require('http');
+var http = require('http');
 //var argv = process.argv;
 var express = require("express");
+
+// Webapp
+
 var app = express();
 app.use(express.logger());
 
@@ -11,6 +14,7 @@ app.get('/', function(request, response) {
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
@@ -25,6 +29,8 @@ if (process.env.ENV == 'PRODUCTION') {
 	var username = config.username;
 	var password = config.password;
 }
+
+// Client XMPP
 
 var cl = new xmpp.Client({
 	jid: username,
@@ -45,13 +51,13 @@ cl.on('stanza',function(stanza) {
 	var body = stanza.getChild('body');
 	if (body) {
 		var message = body.getText();
-		//http.get("http://api.electricimp.com/v1/679dcd70d05fc6f8/30ab3df52bbacf21?value=" + message, function(res) {
-		//	console.log(message);
-		//	console.log("Got response: " + res.statusCode);
-//
-		//}).on('error', function(e) {
-		//	console.log("Got error: " + e.message);
-		//});
+		http.get("http://api.electricimp.com/v1/679dcd70d05fc6f8/30ab3df52bbacf21?value=" + message, function(res) {
+			console.log(message);
+			console.log("Got response: " + res.statusCode);
+
+		}).on('error', function(e) {
+			console.log("Got error: " + e.message);
+		});
 	}
 	// Swap addresses...
 		
@@ -59,8 +65,8 @@ cl.on('stanza',function(stanza) {
 		 delete stanza.attrs.from;
 		 //and send back.
 	if (body) 
-		 console.log("message!");
-		 cl.send(stanza);
+		 console.log("message! " + message);
+		 //cl.send(stanza);
 
 
 
